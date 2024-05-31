dc6c369ae48528e3f61a6f20a8fb4a919cabbbd4
Gabriel.prueba <gabriel-meneghetti@hotmail.com>
2024-05-31 18:15:46 -0300

    avanzando

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import "./css/style.css"
 import './components/header/NavBar'
 import { ItemListContainer } from "./components/ItemListContainer"
 import { Header } from "./components/header/Header"
+import { Footer } from "./components/footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Item } from "./components/Item";
+import { Hero} from "./components/Hero"
 
 function App() {
 
   const valorCarrito = 2;
 
   return (
-    <>
-      <Header valorModificado={valorCarrito}/>
-      <ItemListContainer props={"Acá va a ir el contenido."} />
-    </>
+      <BrowserRouter>      
+        <Header valorModificado={valorCarrito}/>
+        <Routes>          
+          <Route path="/"  element={
+            <>
+              <Hero />
+              <ItemListContainer />            
+            </>
+          }></Route>
+          <Route path="/category/:categoryId"  element={<ItemListContainer />}></Route>          
+          <Route path="/item/:itemId" element={<Item/>}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   )
 }
 
